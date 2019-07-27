5cf957fd6016e8bfc2be2013a8aff96fd416b7ac
Monkeyyy11 <gaileshd@gmail.com>
2019-07-27 22:40:35 +0200

    updated product mongoose schema

--- src/models/product.js
+++ src/models/product.js
@@ -1,8 +1,48 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  text: {
+  id: {
     type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  startPrice: {
+    type: Number,
+    required: true
+  },
+  requirement: {
+    type: String,
+    required: true
+  },
+  sellable: {
+    type: Boolean,
+    required: true
+  },
+  production: {
+    type: String,
+    required: true
+  },
+  productiontime: {
+    type: Number, // Time in minutes per 100m2
+    required: true
+  },
+  productionquantity: {
+    type: Number, // Quantity in minutes per m2
+    required: true
+  },
+  volume: {
+    type: Number,
+    required: true
+  },
+  productlevel: {
+    type: Number,
     required: true
   }
 });

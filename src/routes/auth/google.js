a113481b45734922e59c7a2400106d4c8cdc6662
Monkeyyy11 <gaileshd@gmail.com>
2019-07-27 07:22:23 +0200

    new subdirectory for auth (google)

--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -1,22 +1,9 @@
-// This file’s router holds all your routes. This is the only router that your application has to load at startup.
 var router = require('express').Router();
 const passport = require('passport');
-const auth = require('../middleware/auth');
+const auth = require('../../middleware/auth');
 
 auth(passport);
 router.use(passport.initialize());
-
-router.get('/', (req, res, next) => res.render('index', {
-  loggedIn: req.user
-}));
-
-router.get('/logout', (req, res) => {
-  if (!req.user) {
-    req.logout();
-    req.session = null;
-  }
-  return res.redirect('/');
-});
 
 router.get('/auth/google', passport.authenticate('google', {
   scope: ['https://www.googleapis.com/auth/userinfo.profile']

096fd397a319dd6d26ea1b6a5ea5a9aa0ab10d9b
Monkeyyy11 <gaileshd@gmail.com>
2019-07-28 15:54:36 +0200

    updated userschema

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  googleId: {
+    type: Number, 
+    required: true
+  },
+  id: {
+    type: Number,
+    required: true
+  },
   username: {
     type: String,
     unique: true
+  },
+  avatar: {
+    type: String,
+    required: true
   }
 });
 

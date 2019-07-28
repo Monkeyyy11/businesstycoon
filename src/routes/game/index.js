e633a901ce2868b1fb2c45579afd3718aa7fe32b
Monkeyyy11 <gaileshd@gmail.com>
2019-07-28 15:55:11 +0200

    updated index route for /game

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -1,6 +1,11 @@
 var router = require('express').Router();
+const checkLogIn = require('../../helpers/checkLogIn');
 
-router.get('/', async (req, res, next) => res.render('game/index'));
+router.get('/', async (req, res, next) => {
+  return res.render('game/index', {
+    user: checkLogIn(req, res)
+  });
+});
 
 router.use('/products', require('./products'));
 router.use('/cities', require('./cities'));

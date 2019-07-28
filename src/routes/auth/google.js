e6921278624c543a75c4f3d0fede876541f1eefe
Monkeyyy11 <gaileshd@gmail.com>
2019-07-28 18:49:56 +0200

    added unique id for every user

--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 const passport = require('passport');
 const auth = require('../../middleware/auth');
 const UserModel = require('../../models/user');
+const ServerModel = require('../../models/server');
 
 auth(passport);
 router.use(passport.initialize());
@@ -14,23 +15,31 @@ router.get('/callback',
   passport.authenticate('google', {
     failureRedirect: '/'
   }), async (req, res) => {
-
-    await UserModel.findOne({ googleId: req.session.passport.user.profile.id }).exec((err, result) => {
+    await UserModel.findOne({ googleId: req.session.passport.user.profile.id }).exec(async (err, result) => {
       if (err || !result) {
-        const userObject = {
-          googleId: req.session.passport.user.profile.id,
-          id: 70,
-          username: req.session.passport.user.profile.displayName,
-          avatar: req.session.passport.user.profile._json.picture
-        };
+        await ServerModel.findOne().exec(async (err, result2) => {
+          result2.usercount += 1;
 
-        const user = new UserModel(userObject);
-        user.save((err) => {
-          if (err) return console.error(err);
-          console.log(`Saved new user ${req.session.passport.user.profile.displayName}`);
+          req.session.passport.user.profile.ingameId = result2.usercount;
+
+          await ServerModel.updateOne({}, result2);
+
+          const userObject = {
+            googleId: req.session.passport.user.profile.id,
+            id: result2.usercount,
+            username: req.session.passport.user.profile.displayName,
+            avatar: req.session.passport.user.profile._json.picture
+          };
+
+          const user = new UserModel(userObject);
+          await user.save((err) => {
+            if (err) return console.error(err);
+            console.log(`Saved new user ${req.session.passport.user.profile.displayName} (ID: ${result2.usercount})`);
+          });
         });
+      } else {
+        req.session.passport.user.profile.ingameId = result.id;
       }
-
       req.session.token = req.user.token;
       res.redirect('/game');
     });
